b1b385b72fc0bb9b2b6a6d8345cbfdbaa0a48794
Zac Collier <zacacollier@gmail.com>
2017-03-24 14:40:58 -0500

    Step 3: Add action type

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux';
 
-// 1: Create the Store
 const initialState = {
   min: 0
 }
-const reducer = (state = initialState.min) => {
+const rootReducer = (state = initialState.min, action) => {
   return state;
 }
-const store = createStore(reducer);
 
-// 2: Change the state, read the error
+const store = createStore(
+  rootReducer,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
+
+/* 3: Add a type so that we change the state for real this time
+ *    (and add Redux DevTools support)
+ */
 ReactDOM.render(
   <h1>
     { store.getState() }
   </h1>,
   document.getElementById('root'),
-  document.addEventListener('click', () => store.dispatch())
+  document.addEventListener('click', () => store.dispatch({ type: 'UP' }))
 );
