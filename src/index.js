88792ff61dc9e81c9a00d72c1b898021e323ca08
Zac Collier <zacacollier@gmail.com>
2017-03-24 13:20:57 -0500

    Add files, complete Step 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import { createStore } from 'redux';
+
+// import App from './App';
+
+// 1: Create the Store
+const initialState = {
+  min: 0
+}
+const reducer = (state = initialState.min) => {
+  return state;
+}
+const store = createStore(reducer);
+
 
 ReactDOM.render(
-  <App />,
+  <h1>
+    { store.getState() }
+  </h1>,
   document.getElementById('root')
 );
