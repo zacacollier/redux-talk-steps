74988a01bf13edd57b1ce478a3c417ee9d92164c
Zac Collier <zacacollier@gmail.com>
2017-03-24 22:05:54 -0500

    Step 4: Refactor Reducer

--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import fp from 'lodash/fp';
 import { createStore } from 'redux';
 
 const initialState = {
   min: 0
 }
-const rootReducer = (state = initialState.min, action) => {
-  return state;
+/* 4a: Catch the action type in the Root Reducer
+ *    and return a new State object.
+ *    We can observe the Action in our DevTools...
+ */
+const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'UP':
+      return { ...state, min: ++state.min }
+    default:
+      return state;
+  }
 }
 
 const store = createStore(
   rootReducer,
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
-
-
-/* 3: Add a type so that we change the state for real this time
- *    (and add Redux DevTools support)
+/* 4b: ...but we can't see our data re-rendering
+ *
  */
 ReactDOM.render(
-  <h1>
-    { store.getState() }
-  </h1>,
-  document.getElementById('root'),
-  document.addEventListener('click', () => store.dispatch({ type: 'UP' }))
+    <Provider store={store}>
+      <h1>
+      { fp.toPairs(store.getState()).map(e => (<div key={e[1]}> {e[0]} : {e[1]}</div>)) }
+      </h1>
+    </Provider>,
+    document.getElementById('root'),
+    document.addEventListener('click', () => store.dispatch({ type: 'UP' })),
 );
